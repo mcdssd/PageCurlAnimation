e3460fcb58e0563e1c7db001cd5bd0b5c6c3b9fe
Marina Costa <marinacsantos2@gmail.com>
2025-05-15 15:37:22 -0300

    [Main]: adding Xcode project

---
+++ PageCurlAnimation/PageCurlAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageCurlAnimation
+//
+//  Created by Marina Costa dos Santos on 15/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
